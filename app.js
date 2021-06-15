cb1b3b0c1bce482996e0151ab923518b2ff7de35
Noel Ortiz <67615585+NoelDevs17@users.noreply.github.com>
2021-06-15 14:00:15 -0400

    insertando primeros cambios

---
+++ app.js
@@ -0,0 +1,41 @@
+//Definiendo clases
+class Libro{
+    constructor (titulo, autor, isbn){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+    }
+}
+
+class UI{
+    static mostrarLibros(){
+
+    }
+    static agregarLibroLista(){
+
+    }
+    static eliminarLibro(){
+
+    }
+    static mostrarAlerta(mensaje){
+
+    }
+
+    static limpiarCampos(){
+
+    }
+}
+
+class Datos {
+    static traerLibros(){
+
+    }
+
+    static agregarLibro(libro){
+
+    }
+
+    static removerLirbo(isbn){
+        
+    }
+}

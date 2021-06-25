ef37157048594add3e4134d1de27d038b7f87695
Noel Ortiz <67615585+NoelDevs17@users.noreply.github.com>
2021-06-25 02:46:50 -0400

    agregando metodo mostrar alerta y  posicionamiento de la alerta

--- app.js
+++ app.js
@@ -17,8 +17,16 @@ class UI{
     static eliminarLibro(){
 
     }
-    static mostrarAlerta(mensaje){
+    static mostrarAlerta(mensaje, className){
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(mensaje));
 
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#libro-form');
+        container.insertBefore(div, form);
+
+        setTimeout(()=>document.querySelector('.alert').remove(), 3000);
     }
 
     static limpiarCampos(){
@@ -43,4 +51,16 @@ class Datos {
 //controlar event submit
 document.querySelector('#libro-form').addEventListener('submit', (e) => {
     e.preventDefault();
-});
+
+    //obtener valores de los campos
+    const titulo = document.querySelector('#titulo').value;
+    const autor = document.querySelector('#autor').value;
+    const isbn = document.querySelector('#isbn').value;
+
+
+    if(titulo === '' || autor === '' || isbn === '' ){
+        UI.mostrarAlerta('Por favor ingrese todos los datos','danger');
+    }
+
+    });
+

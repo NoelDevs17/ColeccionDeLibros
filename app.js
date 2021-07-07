2b22e2b65c45cef57e0531189e70c283db04b47a
Noel Ortiz <67615585+NoelDevs17@users.noreply.github.com>
2021-07-07 11:32:27 -0400

    big changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-//Definiendo clases
+//Definición de las clases
 class Libro{
-    constructor (titulo, autor, isbn){
+    constructor(titulo, autor, isbn){
         this.titulo = titulo;
         this.autor = autor;
         this.isbn = isbn;
@@ -9,14 +9,31 @@ class Libro{
 
 class UI{
     static mostrarLibros(){
+        const libros = Datos.traerLibros();
+        libros.forEach((libro) => UI.agregarLibroLista(libro));
+    }
+
+    static agregarLibroLista(libro){
+        const lista = document.querySelector('#libro-list');
+
+        const fila = document.createElement('tr');
+        fila.innerHTML = `
+            <td>${libro.titulo}</td>
+            <td>${libro.autor}</td>
+            <td>${libro.isbn}</td>
+            <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `;
+        
+        lista.appendChild(fila);
 
     }
-    static agregarLibroLista(){
 
+    static eliminarLibro(el){
+        if(el.classList.contains('delete')){
+            el.parentElement.parentElement.remove();
+        }
     }
-    static eliminarLibro(){
 
-    }
     static mostrarAlerta(mensaje, className){
         const div = document.createElement('div');
         div.className = `alert alert-${className}`;
@@ -36,7 +53,7 @@ class UI{
     }
 }
 
-class Datos {
+class Datos{
     static traerLibros(){
         let libros;
         if(localStorage.getItem('libros') === null){
@@ -51,34 +68,46 @@ class Datos {
         const libros = Datos.traerLibros();
         libros.push(libro);
         localStorage.setItem('libros', JSON.stringify(libros));
-
     }
 
-    static removerLirbo(isbn){
-        
+    static removerLibro(isbn){
+        const libros = Datos.traerLibros();
+        console.log(isbn);
+
+        libros.forEach((libro, index) => {
+            if(libro.isbn === isbn){
+                libros.splice(index, 1);
+            }
+        });
+        localStorage.setItem('libros', JSON.stringify(libros));
     }
 }
 
-//controlar event submit
-document.querySelector('#libro-form').addEventListener('submit', (e) => {
+//Carga de la página
+document.addEventListener('DOMContentLoaded',UI.mostrarLibros());
+
+//Controlar el Evento Submit.
+document.querySelector('#libro-form').addEventListener('submit',(e) => {
     e.preventDefault();
 
-    //obtener valores de los campos
+    //Obtener valores de los campos
     const titulo = document.querySelector('#titulo').value;
     const autor = document.querySelector('#autor').value;
     const isbn = document.querySelector('#isbn').value;
 
-
-    if(titulo === '' || autor === '' || isbn === '' ){
-        UI.mostrarAlerta('Por favor ingrese todos los datos','danger');
+    if(titulo === '' || autor === '' || isbn === ''){
+        UI.mostrarAlerta('Por favor ingrese todos los datos', 'danger');
     }else{
-        const libro = new Libro(titulo,autor,isbn);
+        const libro = new Libro(titulo, autor, isbn);
         Datos.agregarLibro(libro);
+        UI.agregarLibroLista(libro);
+        UI.mostrarAlerta('Libro agregado a la colección','success');
         UI.limpiarCampos();
-
     }
+});
 
-
-
-    });
-
+document.querySelector('#libro-list').addEventListener('click', (e) => {
+    UI.eliminarLibro(e.target);
+    Datos.removerLibro(e.target.parentElement.previousElementSibling.textContent);
+    UI.mostrarAlerta('Libro Eliminado','success');
+});
